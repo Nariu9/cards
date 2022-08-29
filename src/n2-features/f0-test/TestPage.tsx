596ac26bdf45ecff42bdd2e7f0bdc63553f3dd3b
Artem Karpukhin <a.karpukhin9@gmail.com>
2022-08-29 13:43:13 +0300

    add universal components to Test page

--- src/n2-features/f0-test/TestPage.tsx
+++ src/n2-features/f0-test/TestPage.tsx
@@ -1,9 +1,74 @@
-import React from 'react';
+import React, {useState} from 'react';
+import SuperInputText from '../../n1-main/m1-ui/common/c1-SuperInputText/SuperInputText';
+import classes from './Test.module.scss';
+import SuperButton from '../../n1-main/m1-ui/common/c2-SuperButton/SuperButton';
+import SuperCheckbox from '../../n1-main/m1-ui/common/c3-SuperCheckbox/SuperCheckbox';
+import SuperEditableSpan from '../../n1-main/m1-ui/common/c4-SuperEditableSpan/SuperEditableSpan';
+import SuperSelect from '../../n1-main/m1-ui/common/c5-SuperSelect/SuperSelect';
+import SuperRadio from '../../n1-main/m1-ui/common/c6-SuperRadio/SuperRadio';
 
 export const TestPage = () => {
+
+    const [inputText, setInputText] = useState<string>('')
+    const error = inputText ? '' : 'error'
+
+    const showAlert = () => {
+        if (error) {
+            alert('type something...')
+        } else {
+            alert(inputText)
+        }
+    }
+
+    const [checkboxStatus, setCheckboxStatus] = useState<boolean>(false)
+    const [spanValue, setSpanValue] = useState<string>('')
+
+    const arr = ['a', 'ab', 'abc', 'abcd', 'abcde']
+    const [value, onChangeOption] = useState(arr[2])
+
     return (
-        <div>
-            Test
+        <div className={classes.column}>
+            <SuperInputText
+                value={inputText}
+                onChangeText={setInputText}
+                onEnter={showAlert}
+                error={error}/>
+            <div className={classes.row}>
+                <SuperButton onClick={showAlert}>
+                    default
+                </SuperButton>
+
+                <SuperButton red>
+                    delete
+                </SuperButton>
+
+                <SuperButton disabled>
+                    disabled
+                </SuperButton>
+            </div>
+            <SuperCheckbox
+                checked={checkboxStatus}
+                onChangeChecked={setCheckboxStatus}>
+                some text
+            </SuperCheckbox>
+            <SuperEditableSpan
+                value={spanValue}
+                onChangeText={setSpanValue}
+                spanProps={{children: spanValue ? undefined : 'enter text...'}}
+            />
+            <SuperSelect
+                options={arr}
+                value={value}
+                onChangeOption={onChangeOption}
+            />
+            <div className={classes.row}><SuperRadio
+                name={'radio'}
+                options={arr}
+                value={value}
+                onChangeOption={onChangeOption}
+            /></div>
+
+
         </div>
     );
 };
