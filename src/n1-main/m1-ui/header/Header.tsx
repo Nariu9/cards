6d7f077d1d2d41400be0678c1caa11fd61601f08
Artem Karpukhin <a.karpukhin9@gmail.com>
2022-08-29 13:41:31 +0300

    replace Path constant with enum

--- src/n1-main/m1-ui/header/Header.tsx
+++ src/n1-main/m1-ui/header/Header.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import {NavLink} from 'react-router-dom';
-import {PATH} from '../pages/Pages';
+import {Path} from '../pages/Pages';
+import classes from './Header.module.scss';
 
 export const Header = () => {
+
+    const activeClass = (props: { isActive: boolean }) => props.isActive ? classes.active : classes.link
+
     return (
-        <div>
-            <NavLink to={PATH.LOGIN}>Login</NavLink>
-            <NavLink to={PATH.REGISTER}>Registration</NavLink>
-            <NavLink to={PATH.PAGE_NOT_FOUND}>404</NavLink>
-            <NavLink to={PATH.PASSWORD_RECOVERY}>Password recovery</NavLink>
-            <NavLink to={PATH.SET_PASSWORD}>New password</NavLink>
-            <NavLink to={PATH.TEST}>Test</NavLink>
+        <div className={classes.container}>
+            <NavLink to={Path.Login} className={activeClass}>Login</NavLink>
+            <NavLink to={Path.Register} className={activeClass}>Registration</NavLink>
+            <NavLink to={Path.PageNotFound} className={activeClass}>404</NavLink>
+            <NavLink to={Path.PasswordRecovery} className={activeClass}>Password recovery</NavLink>
+            <NavLink to={Path.SetPassword} className={activeClass}>New password</NavLink>
+            <NavLink to={Path.Test} className={activeClass}>Test</NavLink>
         </div>
     );
 };
--- src/n1-main/m1-ui/pages/Pages.tsx
+++ src/n1-main/m1-ui/pages/Pages.tsx
@@ -1,32 +1,32 @@
 import React from 'react';
 import {Navigate, Route, Routes} from 'react-router-dom';
-import {Login} from '../../../n2-features/f1-auth/a1-login/Login.';
+import {Login} from '../../../n2-features/f1-auth/a1-login/Login';
 import {Register} from '../../../n2-features/f1-auth/a2-register/Register';
-import {Error404} from '../common/Error404/Error404';
+import {Error404} from '../common/c7-error404/Error404';
 import {PassRecovery} from '../../../n2-features/f1-auth/a4-passRecovery/PassRecovery';
 import {SetPass} from '../../../n2-features/f1-auth/a5-setPass/SetPass';
 import {TestPage} from '../../../n2-features/f0-test/TestPage';
 
-export const PATH = {
-    LOGIN: '/login',
-    REGISTER: '/register',
-    PAGE_NOT_FOUND: 'page-not-found',
-    PASSWORD_RECOVERY: '/password-recovery',
-    SET_PASSWORD: '/set-password',
-    TEST: '/test'
+export enum Path {
+    Login = '/login',
+    Register = '/register',
+    PageNotFound = 'page-not-found',
+    PasswordRecovery = '/password-recovery',
+    SetPassword = '/set-password',
+    Test = '/test'
 }
 
 export const Pages = () => {
     return (
         <div>
             <Routes>
-                <Route path={'/'} element={<Navigate to={PATH.TEST}/>}/>
-                <Route path={PATH.LOGIN} element={<Login/>}/>
-                <Route path={PATH.REGISTER} element={<Register/>}/>
-                <Route path={PATH.PAGE_NOT_FOUND} element={<Error404/>}/>
-                <Route path={PATH.PASSWORD_RECOVERY} element={<PassRecovery/>}/>
-                <Route path={PATH.SET_PASSWORD} element={<SetPass/>}/>
-                <Route path={PATH.TEST} element={<TestPage/>}/>
+                <Route path={'/'} element={<Navigate to={Path.Test}/>}/>
+                <Route path={Path.Login} element={<Login/>}/>
+                <Route path={Path.Register} element={<Register/>}/>
+                <Route path={Path.PageNotFound} element={<Error404/>}/>
+                <Route path={Path.PasswordRecovery} element={<PassRecovery/>}/>
+                <Route path={Path.SetPassword} element={<SetPass/>}/>
+                <Route path={Path.Test} element={<TestPage/>}/>
                 <Route path={'/*'} element={<Error404/>}/>
             </Routes>
         </div>
